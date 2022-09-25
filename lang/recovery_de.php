448755d15ab225ccca8b3e61eb843419b21e3cc5
gizmore <gizmore@wechall.net>
2022-09-25 09:08:20 +0200

    '7.0.1-r1606 cli fixes'

--- lang/recovery_de.php
+++ lang/recovery_de.php
@@ -1,22 +1,24 @@
 <?php
-return array(
-# Sidebar
-'btn_recovery' => 'Passwort Vergessen?',
-
-# Forgot?
-'mt_recovery_form' => 'Passwort zurücksetzen',
-'msg_recovery_mail_sent' => 'Wir haben Ihnen eine E-Mail gesendet die weitere Anweisungen enthält.',
-'err_email_or_login' => 'Bitte geben Sie an welches Konto Sie zurücksetzen wollen.',
+namespace GDO\Recovery\lang;
+return [
 	
-# Change
-'mt_recovery_change' => 'Passwort ändern',
-'new_password' => 'Neues Passwort',
-'p_recovery_change' => 'Sie dürfen nun ein neues Passwort für Ihr Konto vergeben.',
-'password_retype' => 'Erneut eingeben',
-'err_password_retype' => 'Sie müssen das Passwort zwei mal eingeben.',
-'msg_pass_changed' => 'Ihr Passwort wurde erfolgreich geändert.',
-############################################################
-'mail_subj_recovery' => '[%s] Passwort ändern',
-
-'md_recovery_form' => 'Passwort zurücksetzen',
-);
+	'btn_recovery' => 'Passwort Vergessen?',
+	
+	# Forgot?
+	'mt_recovery_form' => 'Passwort zurücksetzen',
+	'msg_recovery_mail_sent' => 'Wir haben Ihnen eine E-Mail gesendet die weitere Anweisungen enthält.',
+	'err_email_or_login' => 'Bitte geben Sie an welches Konto Sie zurücksetzen wollen.',
+		
+	# Change
+	'mt_recovery_change' => 'Passwort ändern',
+	'new_password' => 'Neues Passwort',
+	'p_recovery_change' => 'Sie dürfen nun ein neues Passwort für Ihr Konto vergeben.',
+	'password_retype' => 'Erneut eingeben',
+	'err_password_retype' => 'Sie müssen das Passwort zwei mal eingeben.',
+	'msg_pass_changed' => 'Ihr Passwort wurde erfolgreich geändert.',
+	############################################################
+	'mail_subj_recovery' => '[%s] Passwort ändern',
+	
+	'md_recovery_form' => 'Passwort zurücksetzen',
+	'err_recovery_needs_a_mail' => '%s\'s account cannot reset their password because it has no mail.',
+];
--- lang/recovery_en.php
+++ lang/recovery_en.php
@@ -1,22 +1,25 @@
 <?php
-return array(
-# Sidebar
-'btn_recovery' => 'Forgot Password?',
-
-# Forgot?
-'mt_recovery_form' => 'Password Recovery',
-'msg_recovery_mail_sent' => 'We have sent you an E-Mail with instructions.',
-'err_email_or_login' => 'Please provide information on which account you would like to recover.',
+namespace GDO\Recovery\lang;
+return [
+	# Sidebar
+	'btn_recovery' => 'Forgot Password?',
 	
-# Change
-'mt_recovery_change' => 'Password Reset',
-'new_password' => 'New Password',
-'p_recovery_change' => 'You may now choose a new password for your account.',
-'password_retype' => 'Retype it',
-'err_password_retype' => 'You did not retype your new password correctly.',
-'msg_pass_changed' => 'Your password has been changed successfully.',
-############################################################
-'mail_subj_recovery' => '[%s] Password Recovery',
-############################################################
-'md_recovery_form' => 'Reset Password',	
-);
+	# Forgot?
+	'mt_recovery_form' => 'Password Recovery',
+	'msg_recovery_mail_sent' => 'We have sent you an E-Mail with instructions.',
+	'err_email_or_login' => 'Please provide information on which account you would like to recover.',
+		
+	# Change
+	'mt_recovery_change' => 'Password Reset',
+	'new_password' => 'New Password',
+	'p_recovery_change' => 'You may now choose a new password for your account.',
+	'password_retype' => 'Retype it',
+	'err_password_retype' => 'You did not retype your new password correctly.',
+	'msg_pass_changed' => 'Your password has been changed successfully.',
+	############################################################
+	'mail_subj_recovery' => '[%s] Password Recovery',
+	############################################################
+	'md_recovery_form' => 'Reset Password',
+		
+	'err_recovery_needs_a_mail' => '%s\'s account cannot reset their password because it has no mail.',
+];
